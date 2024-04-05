6459fdf6257ca84ab54b2119f5a92ef071b12f5a
Edwin Guel <edwinfabianguel62@gmail.com>
2024-04-05 11:39:55 -0600

    updated

---
+++ ejercicio 1/variables.js
@@ -0,0 +1,19 @@
+
+// Add your code here
+// Declaración de la variable myName
+var myName = "fabian";
+
+// Declaración e inicialización de la variable myAge en la misma línea
+var myAge = 2023 - 2004;
+
+console.log("Hola, mi nombre es", myName, "y tengo", myAge, "años");
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+const para1 = document.createElement('p');
+para1.textContent = myName;
+const para2 = document.createElement('p');
+para2.textContent = myAge;
+section.appendChild(para1);
+section.appendChild(para2);
+    
